a207882d726354b1f36d69fcb7b86eb4bde692fc
ivababukova <ibabukova@gmail.com>
2015-01-21 15:21:50 +0000

    server and client talking to each other, working

--- hello_client.c
+++ hello_client.c
@@ -5,31 +5,108 @@
 // command hello client bo720-1-01.dcs.gla.ac.uk.
 
 #include <stdio.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netdb.h>
 
-struct sockaddr_in addr;
-// ...
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
-// *
+#define BUFLEN 1500
 
-// creating a socket:
-int fd = socket(AF_INET, SOCK_STREAM, 0); // returns an integer known as a file descriptor that identifies the newly created socket
 
-if (fd == -1) {
-    return -1; // an error has occured
+int main (int argc, char *argv[]) {
+
+    
+    struct addrinfo hints, *ai, *ai0;
+    struct hostent *server;
+    int status, fd, rcount;
+    ssize_t i;
+    char buf[BUFLEN];
+    
+
+    // creating a socket:
+    fd = socket(AF_INET, SOCK_STREAM, 0); // returns an integer known as a file descriptor that identifies the newly created socket
+    if (fd == -1) {
+        perror ("error opening socket\n");
+        exit (1);
+    }
+    
+
+    // connect() via a DNS query:
+    // struct sockaddr_in addr;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = PF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM; // Want a TCP socket
+
+    if ((status = getaddrinfo (argv[1], "5000", &hints, &ai0) ) != 0) {
+        printf("Error: unable to lookup IP address: %s", gai_strerror(status));
+    }
+
+    // ai0 is a pointer to the head of a linked list of struct addrinfo
+    // values containing the possible addresses of the server; interate
+    // through the list, trying to connect to each turn, stopping when
+    // a connection succeeds:
+    for (ai = ai0; ai != NULL; ai = ai->ai_next) {
+        fd = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
+        if (fd == -1) {
+            // Unable to create socket, try next address in list
+            continue;
+            //perror ("Unable to connect1.\n");
+        }
+
+        if (connect(fd, ai->ai_addr, ai->ai_addrlen) == -1) {
+            // couldn’t connect to the address, try next in list
+            close(fd);
+            continue;
+        }
+        
+        break;
+    }
+
+    if (ai == NULL) {
+        // Unsuccessfully connected, handle the error
+        perror ("Unable to connect.\n");
+        exit (1);
+    }
+    
+    
+    // write():
+    
+    rcount = write (fd, buf, strlen(buf));
+    if (rcount == -1) {
+        perror ("error occured while writing to the socket.\n");
+        exit (1);
+    }
+    
+    // read() server response:
+      rcount = read (fd, buf, BUFLEN);  // reads up to BUFLEN bytes of data from connection -     blocks until data available
+    
+    if (rcount == -1) {
+        // an error has occured
+        perror ("Error occured while reading data from the connection.\n");
+        exit (1);
+    }
+    
+    for (i = 0; i<rcount; i++) {
+        printf ("%c", buf[i]);
+    }
+
+    // close():
+/*    int statusa;
+    statusa = close(fd);
+*/
+    close(fd);
+    freeaddrinfo(ai0);
+    return 0;
 }
 
 
 
-
-// *
-
-
-if (connect (fd, (struct sockaddr *) &addr, sizeof(addr)) == -1) {
-    //... error has occured
-} 
-
-// ...
 
 
--- hello_server.c
+++ hello_server.c
@@ -1,64 +1,82 @@
 //  The server should listen on TCP port 5000 for incoming connections. It should accept
 //  the first connection made, read all the data it can from that connection, print that data to the screen,
 //  close the connection, and exit.
+// * socket(), bind(), listen(), accept(), recv(), send(), close()
 #include <stdio.h>
-// to use the socket API:
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/select.h>  // tels which group of sockets has data available to read, this is for handling multiple sockets
+#include <unistd.h>
 
-// *
-
-// make the newly created socket into a TCP server:
-// bind the socket into an appropriate port:
-
-if (bind (fd, ))
-
-
-// *
+int main (int argc, char *argv[]) {
+struct sockaddr_in addr;
 
 
-
-struct addrinfo hints;
-struct addrinfo *ai0;
-int i;
-
-// ...
-
-memset(&hints, 0, sizeof(hints));
-hints.ai_family = PF_UNSPEC;
-// Can use either IPv4 or IPv6
-hints.ai_socktype = SOCK_STREAM; // Want a TCP socket
-if ((i = getaddrinfo("www.example.com", "5000", &hints, &ai0)) != 0) {
-    printf("Error: unable to lookup IP address: %s", gai_strerror(i));
-// ...
-}
-
-// ai0 is a pointer to the head of a linked list of struct addrinfo
-// values containing the possible addresses of the server; interate
-// through the list, trying to connect to each turn, stopping when
-// a connection succeeds:
-struct addrinfo *ai;
-
-int fd;  // create a socket
-for (ai = ai0; ai != NULL; ai = ai->ai_next) {
-    fd = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
+    // socket():
+    int fd = socket (AF_INET, SOCK_STREAM, 0); // create a new communication end point
     if (fd == -1) {
-        // Unable to create socket, try next address in list
-        continue;
+        return -1; // an error occured
     }
-    if (connect(fd, ai->ai_addr, ai->ai_addrlen) == -1) {
-        // couldn’t connect to the address, try next in list
-        close(fd);
-        continue;
+
+    
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons (5000);
+    addr.sin_addr.s_addr = htonl (INADDR_ANY);
+    
+
+    // bind():
+    // make the newly created socket into a TCP server:
+    // bind the socket into an appropriate port:
+    if (bind (fd, (struct sockaddr *) &addr, sizeof(addr) ) == -1) {  // attach a local address  to a socket
+        return -1; // an error occured
     }
-    break; // successfully connected
-}
-if (ai != NULL) {
-    // Successfully connected: fd is a file descriptor of a socket
-    // connected to the server; use the connection
+
+
+    // listen():
+    int listens;
+    listens = listen (fd, 10); 
+    if (listens == -1) {
+        return -1;  // error has occured
+    }
+
+
+    // accept();
+    // accepts a connection,  returns new file descriptor for the connection (connfd) and   clientaddr (cliaddr) 
+    int connfd;
+    struct sockaddr_in cliaddr;
+    socklen_t cliaddrlen;
+    cliaddrlen = sizeof(cliaddr);
+
+    connfd = accept (fd, (struct sockaddr *) &cliaddr, &cliaddrlen);
+    if (connfd == -1) {
+        return -1; // error has occured
+    }
+
+
+    // read():
+
+
+    // write():
+    char data[] = "Hello, world!"; 
+    int datalen, count;
+
+    datalen = strlen(data);
+    count = write(connfd, data, datalen); 
+    if (count == -1) {
+        perror ("Error while writing.\n");
+    }
+
     // ...
-} else {
-    // Couldn’t connect to any of the addresses, handle failure...
-    // ...
+
+
+    // close():
+    int status;
+    status = close(connfd); // closing the socket, status = 0 if successful, -1 if error
+
 }
+
+
 
